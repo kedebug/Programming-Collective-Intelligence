14d2113cc615e9b9da769b59b659393208d3da5f
kedebug <sunweiqq@gmail.com>
2013-07-25 00:22:03 +0800

    Chapter02

--- Chapter02/recommendation.py
+++ Chapter02/recommendation.py
@@ -34,15 +34,37 @@ def sim_distance(prefs, person1, person2):
      
     return 1 / (1 + sum_of_sequares)
 
+# http://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
+def sim_pearson(prefs, person1, person2):
+    similarity = {}
+    
+    for it in prefs[person1]:
+        if it in prefs[person2]:
+            similarity[it] = 1
+    
+    n = len(similarity)
+    if n == 0: return 0
+
+    sum1 = sum([prefs[person1][it] for it in similarity])
+    sum2 = sum([prefs[person2][it] for it in similarity])
+    
+    sum1_square = sum([pow(prefs[person1][it], 2) for it in similarity])
+    sum2_square = sum([pow(prefs[person2][it], 2) for it in similarity])
+    
+    person_sum = sum([prefs[person1][it] * prefs[person2][it] for it in similarity])
+    
+    num = person_sum - (sum1 * sum2) / n
+    den = sqrt((sum1_square - pow(sum1, 2)/n) * (sum2_square - pow(sum2, 2) / n))
+     
+    if den == 0: return 0
+    
+    return num / den
+
 def main():
     person1 = 'Lisa Rose'
     person2 = 'Gene Seymour'
-    print sim_distance(critics, person1, person2)
+    print sim_pearson(critics, person1, person2)
     
 if __name__ == '__main__':
     main()    
-    
-    
-    
-    
     
